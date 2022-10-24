ab426a72b130cd5fec55ba5b44a0eacf992ecedc
mmmorshed987 <101981911+mmmorshed987@users.noreply.github.com>
2022-10-24 18:41:15 -0400

    adding a new file to my Branch

--- e-BankingProject/src/test/java/pageTestPackage/Base_Test_Class.java
+++ e-BankingProject/src/test/java/pageTestPackage/Base_Test_Class.java
@@ -7,6 +7,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import utilityPackage.ReadConfiq;
@@ -23,18 +24,32 @@ public class Base_Test_Class {
 	
 	
 	
-	
+	@Parameters("browser")
 	@BeforeClass
-	public void setup()					// setup method for base class
+	public void setup(String br)					// setup method for base class
 	{
+	if(br.equals("chrome")){
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
-		
+//		driver.manage().window().maximize();
+//		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+//		driver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
+		}
+	else if(br.equals("firefox"))
+	{
+		WebDriverManager.firefoxdriver();
 	}
-
+	else if (br.equals("ie")) {
+		WebDriverManager.iedriver();
+	}
+	driver.get(baseUrl);
+	
+	
+	
+	
+	}
+	
+		
 	@AfterClass
 	public void tearDown() 			// tear down method for base class
 	{
